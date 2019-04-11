361089821c968a21d7ab06d23ab0248c6f7ebca7
Ericka Zavala <ezavalar_isc@hotmail.com>
2019-04-11 18:01:13 -0500

    Correción con blancos.

--- mod1_types_sentences.js
+++ mod1_types_sentences.js
@@ -1,3 +1,4 @@
+console.log();
 let hour= new Date().getHours();
 if(hour>=7 && hour<=12)
 	console.log("Good morning, it's "+hour+" o'clock");
@@ -5,20 +6,20 @@ else if(hour>=13 && hour<=22)
 	console.log("Good afternoon, it's "+hour+" o'clock");
 else
 	console.log("Good night, it's "+hour+" o'clock");
-
+console.log();
 console.log("Number PI with 6 decimals: "+(Math.PI).toFixed(6));
-
+console.log();
 for(var num=0; num<=22; num++)
 {
 	console.log(num+" dec= "+(num).toString(16)+" hex= "+(num).toString(8)+" oct= "+(num).toString(2)+" bin");
 }
-
-for(var num=1; num<=21; num++)
+console.log();
+for(let num=1; num<=21; num++)
 {
-	if(num%3==0)
-		console.log(num+" dec= "+(num).toString(16)+" hex= "+(num).toString(8)+" oct= "+(num).toString(2)+" bin");
+		if(num%2!=0 && !(num>=10 && num<=20))
+			console.log(num+" dec= "+(num).toString(16)+" hex= "+(num).toString(8)+" oct= "+(num).toString(2)+" bin");
 }
-
+console.log();
 console.log("Hi in Chinese is written as: \u55e8\uff0c\u4f60\u597d\u5417");
-
+console.log();
 console.log("The program has finished");
