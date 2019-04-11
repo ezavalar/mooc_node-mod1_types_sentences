5c34dc45bee687ed91fb380d4f95e73ae2c83508
Ericka Zavala <ezavalar_isc@hotmail.com>
2019-04-11 18:29:09 -0500

    Correción numeración

--- mod1_types_sentences.js
+++ mod1_types_sentences.js
@@ -1,5 +1,6 @@
 console.log();
 let hour= new Date().getHours();
+let num;
 if(hour>=7 && hour<=12)
 	console.log("Good morning, it's "+hour+" o'clock");
 else if(hour>=13 && hour<=22)
@@ -9,15 +10,15 @@ else
 console.log();
 console.log("Number PI with 6 decimals: "+(Math.PI).toFixed(6));
 console.log();
-for(var num=0; num<=22; num++)
+for(num=0; num<=22; num++)
 {
 	console.log(num+" dec= "+(num).toString(16)+" hex= "+(num).toString(8)+" oct= "+(num).toString(2)+" bin");
 }
 console.log();
-for(let num=1; num<=21; num++)
+for(num=1; num<=21; num+=2)
 {
-		if(num%2!=0 && !(num>=10 && num<=20))
-			console.log(num+" dec= "+(num).toString(16)+" hex= "+(num).toString(8)+" oct= "+(num).toString(2)+" bin");
+	if(!(num>=10 && num<=20))
+		console.log(num+" dec= "+(num).toString(16)+" hex= "+(num).toString(8)+" oct= "+(num).toString(2)+" bin");
 }
 console.log();
 console.log("Hi in Chinese is written as: \u55e8\uff0c\u4f60\u597d\u5417");
